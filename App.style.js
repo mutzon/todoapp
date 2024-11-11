c566df03ca65f9609d7b939c8a84eb34c969acfb
mutzon <martinutzon@gmail.com>
2024-11-11 17:33:42 +0100

    layot_40

---
+++ App.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#f9f9f9",
+  },
+  header: { flex: 1, backgroundColor: "red" },
+
+  body: { flex: 5, backgroundColor: "blue" },
+
+  footer: { height: 70, backgroundColor: "green" },
+});
